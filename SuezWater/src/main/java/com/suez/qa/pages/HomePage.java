cd6249e6c087805c14027e18b0c2ca8c63b4a7b0
Himanshu <himanshu.shekhar@melity.com>
2019-08-28 18:28:06 +0530

    Added xpath

--- SuezWater/src/main/java/com/suez/qa/pages/HomePage.java
+++ SuezWater/src/main/java/com/suez/qa/pages/HomePage.java
@@ -1,10 +1,25 @@
 package com.suez.qa.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 import com.suez.qa.base.SuezBase;
 
 public class HomePage extends SuezBase {
 	
 	//Page Factory - OR:
+	@FindBy(xpath=".//a [text () = 'Sign In']")
+	WebElement sign_btn;
+	
+	@FindBy(xpath=".//a [text () = 'Register']")
+	WebElement Register_btn;
+	
+	//Initializing the Page Objects:
+		public HomePage(){
+			PageFactory.initElements(driver, this);
+		}
+	
 	
 
 }
--- SuezWater/src/main/java/com/suez/qa/pages/RegistrationPage.java
+++ SuezWater/src/main/java/com/suez/qa/pages/RegistrationPage.java
@@ -1,10 +1,20 @@
 package com.suez.qa.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 import com.suez.qa.base.SuezBase;
 
 public class RegistrationPage extends SuezBase {
 	
 	//Page Factory - OR:
+	
+	@FindBy(xpath = ".//a [text () = 'Residential']")
+	WebElement Residential;
+	
+	@FindBy(xpath = ".//a [text () = 'Business']")
+	WebElement Business;
+	
 	
 
 }
